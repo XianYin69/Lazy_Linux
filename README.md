df7e241d9d1cd621fdcdd747db059e8f9c1e426c
XianYin69 <www324608yf@outlook.com>
2025-09-17 21:09:05 +0800

    update-2

--- README.md
+++ README.md
@@ -4,6 +4,24 @@ LL(Lazy_Linux， 慵懒Linux！) 是一个用于偷懒的Linux工具集，旨在
 
 ---
 
+## 包含的工具
+
+- 备份和恢复你需要的文件
+   - backup.sh    
+   - restore.sh   
+- 初期配置你的系统
+   - chinesization.sh   
+   - init_git.sh     
+- 配置你的系统 
+   - super_clean_old_kernel.sh(Only on fedora)    
+   - nvidia_driver_installer(Only on fedora)    
+- 软件一键安装
+   - software_installer.sh 
+- 安卓模拟器配置
+   - waydroid_installer 
+   - apk_installer.sh
+---
+
 ## 注意事项
 > 由于本人精力有限且主力系统是Fedora,故只开发了用于Fedora的脚本（部分脚本可以用于其他发行版）。  
 
@@ -25,3 +43,10 @@ LL(Lazy_Linux， 慵懒Linux！) 是一个用于偷懒的Linux工具集，旨在
 
 ## 许可证
 本项目基于 MIT 许可证开源，详情请参阅 [LICENSE](./LICENSE)。
+
+---
+
+## 贡献者
+<a herf="https://github.com/XianYin69/Lazy_Linux/graphs/contributors">
+   <img img src="https://contrib.rocks/image?repo=XianYin69/Lazy_Linux" />
+</a>
--- lazy_linux.sh
+++ lazy_linux.sh
@@ -7,7 +7,15 @@
 # 日期：9-10-2025 
 # =================================================================================================
 
-source "./var/state/STATE.sh"
+#定义行为
+source_shell() {
+    source "./var/index/filepath.sh"
+    source $STATE_PATH
+}
+
+source_shell_end() {
+    source "./var/index/filepath.sh"
+}
 
 OS_TYPE=""
 KERNEL_VERSION=""
@@ -15,49 +23,53 @@ SESSION_TYPE=""
 
 #系统检测
 system_check() {
+    source_shell
     source "/etc/os-release"
     OS_TYPE=$ID
-    sed -i "s/OS_TYPE=.*/OS_TYPE=$OS_TYPE/g" ./stack/state/STATE.sh
+    sed -i "s/OS_TYPE=.*/OS_TYPE=$OS_TYPE/g" $STATE_PATH
     KERNEL_VERSION=$(uname -r)
-    sed -i "s/KERNEL_VERSION=.*/KERNEL_VERSION=$KERNEL_VERSION/g" ./stack/state/STATE.sh
+    sed -i "s/KERNEL_VERSION=.*/KERNEL_VERSION=$KERNEL_VERSION/g" $STATE_PATH
     SESSION_TYPE=$XDG_SESSION_TYPE
-    sed -i "s/SESSION_TYPE=.*/SESSION_TYPE=$SESSION_TYPE/g" ./stack/state/STATE.sh
+    sed -i "s/SESSION_TYPE=.*/SESSION_TYPE=$SESSION_TYPE/g" $STATE_PATH
     source "./stack/state/STATE.sh"
     #检测是否是TTY模式
     if [ $SESSION_TYPE = "tty" ]; then
         TTY_MODE="Y"
-        sed -i "s/TTY_MODE=.*/TTY_MODE=$TTY_MODE/g" ./stack/state/STATE.sh
+        sed -i "s/TTY_MODE=.*/TTY_MODE=$TTY_MODE/g" $STATE_PATH
     else
         TTY_MODE="N"
-        sed -i "s/TTY_MODE=.*/TTY_MODE=$TTY_MODE/g" ./stack/state/STATE.sh
+        sed -i "s/TTY_MODE=.*/TTY_MODE=$TTY_MODE/g" $STATE_PATH
     fi
+    source_shell_end
 }
 
 #语言选择
 select_launcher_language() {
+    source_shell
     while :
     do
         echo  "Please choose your language:"
         echo  "1.中文"
         echo  "2.English"
         read -p ":" LAUNCHER_LANGUAGE
-        if [ "$LAUNCHER_LANGUAGE" -eq 1 ]
+        if [ "$LAUNCHER_LANGUAGE" -eq 1 ]; 
         then
-            sed -i "s/STATE_LANG=.*/STATE_LANG=ch/g" ./stack/state/STATE.sh
+            sed -i "s/STATE_LANG=.*/STATE_LANG=ch/g" $STATE_PATH
             break
-        elif [ "$LAUNCHER_LANGUAGE" -eq 2 ]
+        elif [ "$LAUNCHER_LANGUAGE" -eq 2 ]; 
         then
-            sed -i "s/STATE_LANG=.*/STATE_LANG=en/g" ./stack/state/STATE.sh
+            sed -i "s/STATE_LANG=.*/STATE_LANG=en/g" $STATE_PATH
             break
         else
             echo "You must select one!"
         fi
     done
-    source "./stack/state/STATE.sh"
+    source_shell_end
 }
 
 #语言初始化
 language_choose() {
+    source_shell
     if [ "$TTY_MODE" = "N" ]
     then
         if [ "$STATE_LANG" = "ch" ]; then
@@ -72,10 +84,12 @@ language_choose() {
     else
         source "./lang/en-US-info.sh"
     fi
+    source_shell_end
 }
 
 #子菜单
 initialize_toolkit() {
+    source_shell
     local choice
     while :
     do
@@ -95,9 +109,11 @@ initialize_toolkit() {
             ;;
         esac
     done
+    source_shell_end
 }
 
 linux_configurer() {
+    source_shell
     local choice
     if [[ $OS_TYPE == "fedora" || $OS_TYPE == "centos" || $OS_TYPE == "rhel" ]]
     then
@@ -123,9 +139,11 @@ linux_configurer() {
         log_error "仅支持Fedora系发行版！！！(Only Fedora-based distributions are supported!!!)"
         exit 1
     fi
+    source_shell_end
 }
 
 backup_and_restore() {
+    source_shell
     local choice
     while :
     do
@@ -145,9 +163,11 @@ backup_and_restore() {
             ;;
         esac
     done
+    source_shell_end
 }
 
 software_installer() {
+    source_shell
     local choice
     while :
     do
@@ -161,9 +181,11 @@ software_installer() {
             LAZY_LINUX_SH_SOFTWARE_INSTALLER_CHOICE_ERROR
         fi
     done
+    source_shell_end
 }
 
 waydroid() {
+    source_shell
     local choice
     while :
     do
@@ -171,7 +193,7 @@ waydroid() {
         read -p ":" choice
         case $choice in
             1)
-                sudo bash ./lib/waydroid/waydroid_installer/waydroid_installer.sh
+                sudo bash ./lib/waydroid/waydroid_installer/installer.sh
                 break
             ;;
             2)
@@ -183,10 +205,12 @@ waydroid() {
             ;;
         esac
     done
+    source_shell_end
 }
 
 #菜单
 script_choice() {
+    source_shell
     local choice
     while true
     do
@@ -218,14 +242,17 @@ script_choice() {
             ;;
         esac 
     done
+    source_shell_end
 }
 
 
 #使用次数增加
 time_plus() {
+    source_shell
     local time=$(awk -F '=' '/TIME/{print $2}' ./stack/state/STATE.sh)
     time_plus=$(($time+1))
     sed -i "s/TIME=.*/TIME=$time_plus/g" ./stack/state/STATE.sh
+    source_shell_end
 }
 
 #主函数
