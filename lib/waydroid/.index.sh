b4e3c253a15af565893623fe16440f5765ef58b2
XianYin69 <www324608yf@outlook.com>
2025-09-26 20:43:56 +0800

    update-12-7-3

--- lib/waydroid/.index.sh
+++ lib/waydroid/.index.sh
@@ -2,7 +2,7 @@
 SUP_WAYDROID_HOME_PATH="../"
 WAYDROID_HOME_PATH="./"
 
-WAYDROID_CONFIGURER_FOLDER_PATH="waydroid_configurer/.index.sh"
-WAYDROID_INSTALLER_FOLDER_PATH="waydroid_installer/.index.sh"
+WAYDROID_CONFIGURER_FOLDER_PATH_INDEX="waydroid_configurer/.index.sh"
+WAYDROID_INSTALLER_FOLDER_PATH_INDEX="waydroid_installer/.index.sh"
 
 #文件索引
